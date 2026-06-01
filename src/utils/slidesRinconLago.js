8416f830f41a8c5a2c4e3df2b2d9dfa7a4a53acc
joburbanop <joburbanop589@gmail.com>
2026-06-01 17:02:35 -0500

    slider de imagene en codigo

--- src/utils/slidesRinconLago.js
+++ src/utils/slidesRinconLago.js
@@ -1,6 +1,11 @@
-import img1 from '../assets/rincon_lago/Rincon_lago_galeria7.webp';
-import img2 from '../assets/rincon_lago/Rincon_lago_galeria4.webp';
-import img3 from '../assets/rincon_lago/Rincon_lago_galeria3.webp';
+import img1 from '../assets/rincon_lago/slider/Slider_1_real.webp';
+import img2 from '../assets/rincon_lago/slider/Slider_1_render.webp';
+import img3 from '../assets/rincon_lago/slider/Slider_2_real.webp';
+import img4 from '../assets/rincon_lago/slider/Slider_2_render.jpg';
+import img5 from '../assets/rincon_lago/slider/Slider_3_real.webp';
+import img6 from '../assets/rincon_lago/slider/Slider_3_render.jpg';
+import img7 from '../assets/rincon_lago/slider/Slider_4_real.webp';
+import img8 from '../assets/rincon_lago/slider/slider_4_render.png';
 
 const slidesRinconLago = [
     {
@@ -17,6 +22,31 @@ const slidesRinconLago = [
         image: img3,
         title: 'slide3_title',
         subtitle: 'slide3_subtitle'
+    },
+    {
+        image: img4,
+        title: 'slide1_title',
+        subtitle: 'slide1_subtitle'
+    },
+    {
+        image: img5,
+        title: 'slide2_title',
+        subtitle: 'slide2_subtitle'
+    },
+    {
+        image: img6,
+        title: 'slide3_title',
+        subtitle: 'slide3_subtitle'
+    },
+    {
+        image: img7,
+        title: 'slide1_title',
+        subtitle: 'slide1_subtitle'
+    },
+    {
+        image: img8,
+        title: 'slide2_title',
+        subtitle: 'slide2_subtitle'
     }
     ];
 
